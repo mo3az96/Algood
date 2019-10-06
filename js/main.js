fa7c381c5386477acb34fb8a47a0adbbcc60bd1e
mo3az.abdellatif <mo3az.abdellatif@gmail.com>
2019-10-07 01:36:56 +0200

    a7a

--- js/main.js
+++ js/main.js
@@ -124,7 +124,7 @@ $(document).ready(function () {
         $(".products-ul").addClass("mopanel");
         $(".nav-foot-header").addClass("moaccordion");
         $(".nav-foot").addClass("mopanel");
-        $(".slider-cont").removeClass("container");
+        //$(".slider-cont").removeClass("container");
     }
 
     // Accordion
